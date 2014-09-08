28d4c34b40454b8ec4f44cc1d56a4136199a00ee
doubletong <twotong@gmail.com>
2014-09-08 23:38:48 +0800

    update

--- js/jQuery.bbiTab.js
+++ js/jQuery.bbiTab.js
@@ -1,11 +1,20 @@
 (function($) {
+   
+    
     //index 为默认索引
-   $.fn.bbiTab = function(index){
+   $.fn.bbiTab = function(opts){
+       
+        var defaults = {
+            currentIndex: 1 // 当前索引
+        };
+        var options = $.extend(defaults, opts);
+       
        var lis = $(this).find('header.tab-header li');
        var tabs = $(this).find('div.tab-body section.tab-content');
+       var i = options.currentIndex-1;
        
-       lis.eq(index).addClass("active");
-       tabs.hide().eq(index).show();
+       lis.eq(i).addClass("active");
+       tabs.hide().eq(i).show();
        
        lis.children("a").hover(function(){
            $(this).closest("li").addClass('active').siblings().removeClass("active");
@@ -13,8 +22,9 @@
             $(id).show().siblings().hide();           
        }).click(function(e){
             e.preventDefault();
-       });
-     
+       });     
    }
+   
+   
     
 })(jQuery);
